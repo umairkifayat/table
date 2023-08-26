49983ad998361ee77325ccc4c28475dc7f99419c
umair kifayat <umairkifayat3@gmail.com>
2023-08-26 12:12:20 -0700

    table

---
+++ app.js
@@ -0,0 +1,15 @@
+const num = +prompt("enter your number a table")
+if (num) {
+console.log(num + "* 10 =" + num*10 ).innerHTML;
+console.log(num + "* 9 =" + num*9 ).innerHTML;
+console.log(num + "* 8 =" + num*8 ).innerHTML;
+console.log(num + "* 7 =" + num*7 ).innerHTML;
+console.log(num + "* 6 =" + num*6 ).innerHTML;
+console.log(num + "* 5 =" + num*5 ).innerHTML;
+console.log(num + "* 4 =" + num*4 ).innerHTML;
+console.log(num + "* 3 =" + num*3 ).innerHTML;
+console.log(num + "* 2 =" + num*2 ).innerHTML;
+console.log(num + "* 1 =" + num*1 ).innerHTML;
+} else {
+    alert(" please enter a number");
+}
